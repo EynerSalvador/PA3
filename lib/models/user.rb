6927139b71f27566a126ffba677766d63075dc7c
EynerSalvador <168598767+EynerSalvador@users.noreply.github.com>
2025-04-23 22:11:51 -0500

    Update user.rb

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -1,1 +1,25 @@
+class User
+  attr_reader :id, :username, :email, :role
 
+  def initialize(attributes = {})
+    @id = attributes[:id] || SecureRandom.uuid
+    @username = attributes[:username]
+    @email = attributes[:email]
+    @password_hash = attributes[:password_hash]
+    @role = attributes[:role] || 'member'
+  end
+
+  def authenticate(password)
+    Password.valid?(@password_hash, password)
+  end
+
+  def to_json(*args)
+    {
+      id: @id,
+      username: @username,
+      email: @email,
+      password_hash: @password_hash,
+      role: @role
+    }.to_json(*args)
+  end
+end
