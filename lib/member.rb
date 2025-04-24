d92bf15f4488e7119da425a6c538b659672c63ab
EynerSalvador <168598767+EynerSalvador@users.noreply.github.com>
2025-04-23 22:36:26 -0500

    Update member.rb

--- lib/member.rb
+++ lib/member.rb
@@ -1,19 +1,31 @@
+# lib/models/member.rb
 class Member
-  REQUIRED_FIELDS = %i[name role skills bio].freeze
+  attr_reader :id, :name, :email, :bio, :avatar_url
 
   def initialize(attributes = {})
-    @attributes = attributes.transform_keys(&:to_sym)
-    validate!
+    @id = attributes[:id] || attributes['id']
+    @name = attributes[:name] || attributes['name']
+    @email = attributes[:email] || attributes['email']
+    @bio = attributes[:bio] || attributes['bio']
+    @avatar_url = attributes[:avatar_url] || attributes['avatar_url']
   end
 
+  # Método de clase para crear miembros (requerido por Rakefile)
+  def self.create(db, member_data)
+    new_member = new(member_data)
+    db[:members] ||= []
+    db[:members] << new_member
+    new_member
+  end
+
+  # Método para compatibilidad con JSON
   def to_h
-    @attributes
-  end
-
-  private
-
-  def validate!
-    missing_fields = REQUIRED_FIELDS - @attributes.keys
-    raise "Missing required fields: #{missing_fields.join(', ')}" unless missing_fields.empty?
+    {
+      id: id,
+      name: name,
+      email: email,
+      bio: bio,
+      avatar_url: avatar_url
+    }
   end
 end
