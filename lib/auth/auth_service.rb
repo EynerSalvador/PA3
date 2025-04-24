19c1a13f6436d6115a537323ebbe3a3f530160c3
EynerSalvador <168598767+EynerSalvador@users.noreply.github.com>
2025-04-24 17:09:59 -0500

    Update auth_service.rb

--- lib/auth/auth_service.rb
+++ lib/auth/auth_service.rb
@@ -1,30 +1,20 @@
 require 'json'
-require_relative '../models/user'
-require_relative 'password'
 
 class AuthService
   def initialize(users_file)
     @users_file = users_file
-    @users = load_users
   end
 
   def authenticate(username, password)
-    user = @users.find { |u| u.username == username }
-    user && user.authenticate(password) ? user : nil
+    users = load_users
+    users.find { |u| u['username'] == username && u['password'] == password }
   end
 
   private
 
   def load_users
-    return [] unless File.exist?(@users_file)
-    
-    JSON.parse(File.read(@users_file)).map do |user_data|
-      User.new(
-        user_data['username'],
-        user_data['password_hash'],
-        user_data['email'],
-        user_data['role'] || 'member'
-      )
-    end
+    JSON.parse(File.read(@users_file))
+  rescue
+    []
   end
 end
