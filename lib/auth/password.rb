bc6afef4ac0f817ef5d4dda946514593e4cf6133
EynerSalvador <168598767+EynerSalvador@users.noreply.github.com>
2025-04-23 22:12:27 -0500

    Update password.rb

--- lib/auth/password.rb
+++ lib/auth/password.rb
@@ -1,1 +1,10 @@
+require 'bcrypt'
 
+module Password
+  def self.create(password)
+    BCrypt::Password.create(password).to_s
+  end
+
+  def self.valid?(hash, password)
+    BCrypt::Password.new(hash) == password
+  end
